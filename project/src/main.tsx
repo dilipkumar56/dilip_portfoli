42f87daf6265b25734aae8cf68cdfcf2b0f2fd37
dilipkumar56 <bandarudilipkumar25@gmail.com>
2025-12-13 12:45:53 -0500

    main

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+//here is the main entry point of the application
+// we are using createRoot to render the App component in the root element of the DOM
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
